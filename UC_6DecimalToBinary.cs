8b3fe30a3a54b19af1ee5e36654486917e6288b7
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-20 22:18:12 +0530

    [akarsh] added: Conversion From Decimal To Binary

---
+++ UC_6DecimalToBinary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace N_Unit_Testing
+{
+    class UC_6DecimalToBinary
+    {
+        public void Binary()
+        {
+            Console.WriteLine("Welcome to Binary Convertion ");
+            Console.WriteLine("Enter a Decimal number : ");
+            int dec = Convert.ToInt32(Console.ReadLine());
+
+            int[] binaryArray = new int[20];
+            int i = 0;
+
+            while (dec > 0)
+            {
+                int r = dec % 2;
+                binaryArray[i++] = r;
+                dec = dec / 2;
+            }
+
+            Console.WriteLine("Binary number :");
+
+            for (int j = i - 1; j >= 0; j--)
+
+                Console.Write(binaryArray[j] + "");
+        }
+    }
+}
