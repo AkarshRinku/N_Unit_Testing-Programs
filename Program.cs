8952569594c4fe085c9da7018fa8dbd165c20f56
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-20 18:26:49 +0530

    [akarsh] added: Welcome To N Unit Program

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace N_Unit_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //UC_1VendingMachine vendingmachine = new UC_1VendingMachine();
+            //vendingmachine.Currencycount();
+
+            //UC_2DaysOfWeek daysOfweek = new UC_2DaysOfWeek();
+            //daysOfweek.days();
+
+            //UC_3TemperatureConversion temperatureconversion = new UC_3TemperatureConversion();
+            //temperatureconversion.Tempconv();
+
+            //UC_4MonthlyPayment monthlyPayment = new UC_4MonthlyPayment();
+            //monthlyPayment.monthlypayment();
+
+            //UC_5SqrtNewtonsMethod sqrt = new UC_5SqrtNewtonsMethod();
+            //sqrt.Sqrt();
+
+            //UC_6DecimalToBinary binary = new UC_6DecimalToBinary();
+            //binary.Binary();
+
+            UC_7SwapNibbles swap = new UC_7SwapNibbles();
+            swap.Main();
+
+
+
+
+
+
+
+        }
+    }
+}
