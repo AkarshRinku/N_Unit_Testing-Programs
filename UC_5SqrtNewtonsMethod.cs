0c3161936f0bcd18c8a01cf1f2846fd5094d5c4b
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-20 22:15:44 +0530

    [akarsh] added: To Compute The Square Root.

---
+++ UC_5SqrtNewtonsMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace N_Unit_Testing
+{
+    class UC_5SqrtNewtonsMethod
+    {
+        public void  Sqrt()
+        {
+            Console.WriteLine("Welcome to Newton's method:");
+            Console.WriteLine("Enter the value of c :");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double epsilon = 1e-15;
+            double t = c;
+
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = (c / t + t) / 2.0;
+            }
+
+            Console.WriteLine(t);
+        }
+    }
+}
